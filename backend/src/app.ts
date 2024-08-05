e6b6b6c5b2d6722e36afe7bf2cb44a57c8ac94f9
0B-111 <bitanomri@gmail.com>
2024-08-05 15:31:42 +0300

    add heroku

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,28 @@
-import express from "express";
+import express, { Request, Response } from 'express';
 import bodyParser from "body-parser";
 import productRoutes from "./routes/productRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import cors from "cors";
 import { sequelize } from "./models";
+import path from "path";
 
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname,'../frontend/build')
 const app = express();
-app.use(cors());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  console.log(req.method, req.originalUrl);
-  next();
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use(express.static(buildPath));
+
+app.get("/*",function(req:Request,res:Response){
+  res.sendFile(path.join(_dirname,'../frontend/build/index.html'),function(err){
+    if(err){
+      res.status(500).send(err);
+    }
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 
